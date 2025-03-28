cace98f84d9f1a377bf8aaacc3ca77df33fcb1d6
erickgomez24110237 <a24110237@live.ceti.mx>
2025-03-28 03:29:57 +0000

    practica 2

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # programacion-3E251
+
+c++ main.cpp -o programa -lftxui-component -lftxui-dom -lftxui-screen -std=c++17
--- main.cpp
+++ main.cpp
@@ -1,25 +1,45 @@
 #include "Serie.hpp"
-
+#include <ftxui/screen/screen.hpp>
+#include <ftxui/dom/elements.hpp>
+#include <iostream>
+using namespace std;
+using namespace ftxui;
 
 int main(int argc, char const *argv[])
 {
+  auto pantalla = Screen::Create(
+    ftxui::Dimension::Full(),
+    Dimension::Full()
 
-Serie serienavidena;
+  );
 
-serienavidena.ApagarTodo();
-serienavidena.Imprimir();
-serienavidena.EncenderTodo();
-serienavidena.Imprimir();
-serienavidena.ApagarCantidad(8);
-serienavidena.Imprimir();
-serienavidena.EncenderCantidad(3);
-serienavidena.Imprimir();
-serienavidena.AlternarEncendido();
-serienavidena.Imprimir();
-serienavidena.RecorrerDerecha();
-serienavidena.Imprimir();
-serienavidena.RecorrerIzquierda();
-serienavidena.Imprimir();
+  auto documento = vbox(
+    spinner(21,1)
+  );
+
+  Render(pantalla, documento);
+  pantalla.Print();
+
+
+
+
+
+//Serie serienavidena;
+
+// serienavidena.ApagarTodo();
+// serienavidena.Imprimir();
+// serienavidena.EncenderTodo();
+// serienavidena.Imprimir();
+// serienavidena.ApagarCantidad(8);
+// serienavidena.Imprimir();
+// serienavidena.EncenderCantidad(3);
+// serienavidena.Imprimir();
+// serienavidena.AlternarEncendido();
+// serienavidena.Imprimir();
+// serienavidena.RecorrerDerecha();
+// serienavidena.Imprimir();
+// serienavidena.RecorrerIzquierda();
+// serienavidena.Imprimir();
 
 
 
